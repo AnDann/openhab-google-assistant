21f6bd4eb44a61c023000723f1ecbf40afa7e1be
skimdankish <32886013+skimdankish@users.noreply.github.com>
2018-02-22 11:52:50 -0500

    Add strict equality

--- functions/openhab.js
+++ functions/openhab.js
@@ -422,14 +422,14 @@ function adjustTemperatureWithItems(authToken, request, response, params, curren
 	}
 	
   	// Google Assistant needs (like Alexa) everything in Celsius, we will need to respect what a user has set
-  	var isF = tempUnit ? (tempUnit.toLowerCase() == 'fahrenheit') : false;
+  	var isF = tempUnit ? (tempUnit.toLowerCase() === 'fahrenheit') : false;
   
 	var setValue;
 	setValue = isF ? utils.toF(params.thermostatTemperatureSetpoint) : params.thermostatTemperatureSetpoint;
 
   	//if heatingCoolingMode has a length of 1 (*should* be number...), then convert to something GA can read (off, heat, cool, on, heatcool)
 	if(heatingCoolingMode && heatingCoolingMode.state )
-		curMode = heatingCoolingMode.state.length == 1 ? utils.normalizeThermostatMode(heatingCoolingMode.state) : heatingCoolingMode.state;
+		curMode = heatingCoolingMode.state.length === 1 ? utils.normalizeThermostatMode(heatingCoolingMode.state) : heatingCoolingMode.state;
 
 	var success = function (resp) {
 		var payload = {};
